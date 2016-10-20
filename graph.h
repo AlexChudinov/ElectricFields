7ab8a1a265edacfb1b66103951fc92ac0829195e
Alexey Chudinov <chudinov@inbox.ru>
2016-10-20 13:43:21 +0300

    Rutine

--- graph.h
+++ graph.h
@@ -1,15 +1,28 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <vector>
+
 /**
  * Graph to keep mesh connectivity information
  */
 
 namespace data_structs {
 
+using std::vector;
+
+template<typename label>
 struct graph
 {
+    using mesh_connectivity_type = vector<vector<label>>;
+    mesh_connectivity_type mesh_connectivity;
 
+    graph(){}
+
+    void estable_connection(label i, label j)
+    {
+
+    }
 };
 
 }
